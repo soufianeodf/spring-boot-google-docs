b6ecd117fca5e454700a23343d386aa1f6b2e693
soufianeodf <soufiane.odf@gmail.com>
2020-11-17 20:06:45 +0100

    Create simple spring boot rest api

---
+++ src/main/java/com/uplora/googledocs/GoogleDocsApplication.java
@@ -0,0 +1,13 @@
+package com.uplora.googledocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uplora/googledocs/GoogleDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplora.googledocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
